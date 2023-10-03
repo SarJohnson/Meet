cb3bb3121d89375dfc8a08b6fe5c08c5ae38bb83
SarJohnson <sej17b@my.fsu.edu>
2023-10-02 23:32:21 -0400

    Added Feature 1 Integration testing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import EventList from './components/EventList.js';
 import './App.css';
 import CitySearch from './components/CitySearch.js';
 import NumberOfEvents from './components/NumberOfEvents.js';
+import { useEffect, useState } from 'react';
+import { extractLocations, getEvents } from './api.js';
 
 const App = () => {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
+  const [currentCity, setCurrentCity] = useState("See all cities");
+
+  useEffect(() => {
+    fetchData();
+  }, [currentCity]);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    const filteredEvents = currentCity === "See all cities"?
+    allEvents:
+    allEvents.filter(event => event.location === currentCity)
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  }
+
   return (
     <div className="App">
-      <CitySearch />
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
       <NumberOfEvents />
-      <EventList />
+      <EventList events={events}/>
     </div>
   );
 }
--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-const CitySearch = ({ allLocations }) => {
+const CitySearch = ({ allLocations, setCurrentCity }) => {
     const [showSuggestions, setShowSuggestions] = useState(false);
     const [query, setQuery] = useState("");
     const [suggestions, setSuggestions] = useState([]);
+    useEffect(() => {
+        setSuggestions(allLocations);
+    }, [`{allLocations}`]);
     const handleInputChanged = (event) => {
         const value = event.target.value;
         const filteredLocations = allLocations ? allLocations.filter((location) => {
@@ -16,6 +19,7 @@ const CitySearch = ({ allLocations }) => {
         const value = event.target.textContent;
         setQuery(value);
         setShowSuggestions(false);
+        setCurrentCity(value);
     };
     return (
         <div id="city-search">
