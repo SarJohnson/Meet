73a92e028b7acdf3a0fb5c5b3740e86e381866ad
SarJohnson <sej17b@my.fsu.edu>
2023-10-04 15:48:20 -0400

    Implemented Atatus

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('428e919d589a4cdaaf681e9f23d11d12').install();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
